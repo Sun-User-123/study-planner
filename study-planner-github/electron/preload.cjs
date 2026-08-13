421541f08e6501f3ddcc7e459fa8e4de7b6ebb0b
Sunny <2995562135@qq.com>
2026-08-13 15:30:57 +0800

    Delete study-planner-github directory

--- study-planner-github/electron/preload.cjs
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  apiBase: process.env.PLANNER_API_BASE || 'http://127.0.0.1:8787',
-  openExternal: (url) => ipcRenderer.invoke('open-external', url),
-});
--- study-planner-github/stop-planner.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-setlocal
-powershell -NoProfile -Command "$connections = Get-NetTCPConnection -LocalPort 8787 -State Listen -ErrorAction SilentlyContinue; if ($connections) { $connections | ForEach-Object { Stop-Process -Id $_.OwningProcess -Force } }"
-echo The planner service has been stopped.
-pause
